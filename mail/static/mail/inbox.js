a647b6b43a268d0a51075fb06fc3e28565e0d992
Lance Tian <79405882+fengmclovin@users.noreply.github.com>
2024-03-08 00:31:11 +1100

    Update inbox.js

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -36,5 +36,24 @@ function load_mailbox(mailbox) {
 
 function send_email(event) {
   event.preventDefault();
-  console.log("Email Successfully Sent");
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent');
+    });
+
 }
